555ce390f7ec9d6f8f172e9e7088e235054a0f81
Yeongyung Wi <yeongyung.wi@stud.uni-goettingen.de>
2025-02-02 20:45:24 +0100

    chapter1

---
+++ chapter1/headfirstjava-ch1/BooleanText.java
@@ -0,0 +1,14 @@
+public class BooleanText {
+    public static void main(String[] args){
+        int x = 4 ;
+        while (x > 3) {
+            x--;
+            System.out.println("Current value of x: " + x);
+        }
+        int z = 17;
+        while (z == 17) {
+            System.out.println("z is 17");
+            z++;
+        }
+    }
+}
